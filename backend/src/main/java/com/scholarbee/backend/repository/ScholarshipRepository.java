4db090704f6e2ddc20d2c3866ddf592ee449488d
Yang Jinhyuk <165665548+y-jh107@users.noreply.github.com>
2025-11-29 19:58:34 +0900

    Revert "장학금 크롤링 로직 수정"

--- backend/src/main/java/com/scholarbee/backend/repository/ScholarshipRepository.java
+++
@@ -1,8 +0,0 @@
-package com.scholarbee.backend.repository;
-
-import com.scholarbee.backend.domain.entity.Scholarship;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScholarshipRepository extends JpaRepository<Scholarship, Long> {
-    boolean existsByName(String name);
-}
